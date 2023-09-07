4994cbe9a556833b259d83a768505af778055b86
Ann-JK <annabelkongas@gmail.com>
2023-09-07 12:13:49 +0300

    DbContext

--- ShopTARge22/Program.cs
+++ ShopTARge22/Program.cs
@@ -1,7 +1,13 @@
+using ShopTARge22.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+//TODO
+//builder.Services.AddDbContext<ShopTARge22Context>(options => options.UseSqlServer());
 
 var app = builder.Build();
 
