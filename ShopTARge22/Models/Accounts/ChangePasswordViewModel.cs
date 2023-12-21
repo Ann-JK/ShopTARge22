4d66ab3850a0e139ea0f2835e6efa9c12ab82589
Ann-JK <annabelkongas@gmail.com>
2023-12-21 12:14:08 +0200

    dropdown menu

--- ShopTARge22/Models/Accounts/ChangePasswordViewModel.cs
+++ ShopTARge22/Models/Accounts/ChangePasswordViewModel.cs
@@ -17,7 +17,7 @@ namespace ShopTARge22.Models.Accounts
         [Required]
         [DataType(DataType.Password)]
         [Display(Name = "Confirm new password")]
-        [Compare("NePassword", ErrorMessage = "The new password and confirmation password do not match")]
+        [Compare("NewPassword", ErrorMessage = "The new password and confirmation password do not match")]
         public string ConfirmPassword { get; set; }
     }
 }
